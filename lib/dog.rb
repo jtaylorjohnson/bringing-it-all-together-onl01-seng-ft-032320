0b9bc8d13f75382ae43cb9632a5428c4dd3cbb7e
James Johnson <jtaylorjohnson@gmail.com>
2020-05-03 22:49:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,9 @@ class Dog
   
   attr_accessor :id, :name, :breed 
   
-  def initialize(id:nil, name:, breed:)
-    @id = id
-    @name = name
-    @breed = breed
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
   end
   
   def self.create_table
